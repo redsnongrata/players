2a2a6bb99e2f4c0fe5e63bb2ad8d932d2ecd5708
redsnongrata <mseddadi308@gmail.com>
2024-01-19 16:55:27 +0100

    first commit

---
+++ football-players-app/src/App.js
@@ -0,0 +1,32 @@
+// src/App.js
+import React from 'react';
+import PlayersList from './PlayersList';
+
+function App() {
+  return (
+    <div className="App">
+      <header style={{ textAlign: 'center', padding: '20px', backgroundColor: '#f0f0f0' }}>
+        <h1 style={{ color: '#333' }}>Football Players</h1>
+      </header>
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
+// src/App.js
+import React from 'react';
+import PlayersList from './PlayersList';
+
+function App() {
+  return (
+    <div className="App">
+      <header style={{ textAlign: 'center', padding: '20px', backgroundColor: '#f0f0f0' }}>
+        <h1 style={{ color: '#333' }}>Football Players</h1>
+      </header>
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
